c664a7eb5c8b963ad676df15f33ee0da15e2e2b9
Bladesheng <keadr23@gmail.com>
2023-02-21 15:25:38 +0100

    Use service worker to return cached resources Cache first, fall back to network.

--- src/workers/sw.ts
+++ src/workers/sw.ts
@@ -18,23 +18,38 @@ self.addEventListener("install", (e) => {
 });
 
 // Serve cached content when offline
-// self.addEventListener("fetch", (e) => {
-//   e.respondWith(
-//     (async () => {
-//       const r = await caches.match(e.request);
-//       console.log(`[SW] Fetching resource: ${e.request.url}`);
-//       if (r) {
-//         return r;
-//       }
+self.addEventListener("fetch", (e) => {
+  const url = new URL(e.request.url);
+  const isPrecachedRequest = filesToCache.includes(url.pathname);
+  const isImageRequest = e.request.destination === "image";
 
-//       const response = await fetch(e.request);
-//       const cache = await caches.open(cacheName);
-//       console.log(`[SW] Caching new resource: ${e.request.url}`);
-//       cache.put(e.request, response.clone());
-//       return response;
-//     })()
-//   );
-// });
+  if (isPrecachedRequest || isImageRequest) {
+    e.respondWith(
+      (async () => {
+        console.log(`[SW] Fetching resource: ${e.request.url}`);
+        // go to the cache first and return a cached response if we have one
+        const cache = await caches.open(cacheName);
+        const cachedResponse = await cache.match(e.request);
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        // otherwise, hit the network and add the network response to the cache for later visits
+        const fetchedResponse = await fetch(e.request);
+        console.log(`[SW] Caching new resource: ${e.request.url}`);
+        cache.put(e.request, fetchedResponse.clone());
+        return fetchedResponse;
+      })()
+    );
+  }
+
+  if (url.pathname === "/api/readings/events") {
+    // TODO: handle SSE
+  }
+  if (url.pathname === "/api/readings/range") {
+    // TODO: handle api calls
+  }
+});
 
 // cleanup old unused caches
 self.addEventListener("activate", (e) => {
