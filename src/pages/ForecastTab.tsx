9eba80f4f8cd9c762300be207ade8ef45501f86b
Bladesheng <keadr23@gmail.com>
2023-04-23 13:18:13 +0200

    Create chart with forecast data

--- src/pages/ForecastTab.tsx
+++ src/pages/ForecastTab.tsx
@@ -3,6 +3,8 @@ import React, { useState, useEffect } from "react";
 import { fetchForecast } from "@api/api";
 import { IForecast, ISunrise } from "@api/api";
 
+import ChartForecast from "@components/charts/ChartForecast";
+
 export default function HistoryTab() {
   const [forecast, setForecast] = useState<IForecast[]>([]);
   const [sunrise, setSunrise] = useState<ISunrise[]>([]);
@@ -16,17 +18,15 @@ export default function HistoryTab() {
     })();
   }, []);
 
-  // if you don't have any forecast data available yet (network or server error)
-  // todo: move this to children components
-  if (forecast.length === 0) {
-    return <h1>Načítání...</h1>;
-  }
+  // if (forecast.length === 0) {
+  //   return <h1>Načítání...</h1>;
+  // }
 
   return (
     <section className="forecastTab">
       <h1>Předpověď WIP</h1>
-      <p>{forecast[0].time.toISOString()}</p>
-      <p>{sunrise[0].date.toISOString()}</p>
+
+      <ChartForecast forecast={forecast} />
     </section>
   );
 }
